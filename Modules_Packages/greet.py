9e16c27d2b8788092ec702ee0392d25cfc10e1a5
Raihan Ahmed Dip <raihan.dip@gmail.com>
2022-11-25 17:36:21 +0600

    Created a python file name greet.py and then import that file to  juopyter notebook file named test.ipynb. Also in the greet.py file worked with the main file which will only run when the file will be exucuted directly not when it is imported. Another thing to remember, we need to to close and repoen the file if we change anything in the file which we are importing.

---
+++ Modules_Packages/greet.py
@@ -0,0 +1,11 @@
+peech = "Welcome Programmers!"
+
+def add(x:int, y:int)->int:
+    """
+    It will return x+y
+    """
+    return x+y
+
+# This will execute when this file will be executed directly, not from import
+if __name__ == "__main__":
+    print("Running Main File")
